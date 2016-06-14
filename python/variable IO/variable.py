2921c782ed31dd2619933bb4675bb726b181b143
gzj <987969625@qq.com>
2016-06-14 09:23:47 +0800

    temp

---
+++ python/variable IO/variable.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+int =1
+float = 2.3
+str = "this is string"
+bool_0 =False
+bool_1 =True
+
+#list
+list = [1, 2, 3, 4, 5 ];
+#tuple
+tuple = (1, 2, 3, 4, 5 );
+#dictionary
+dict = {'a': '1111', 'b': '2222', 'c': '3333'}
+
+print(int)
