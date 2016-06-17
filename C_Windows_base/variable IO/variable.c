e8df547e63eba2ecbb36825cd448af0fd1b194d6
gzj <987969625@qq.com>
2016-06-17 10:06:32 +0800

    temp

---
+++ C_Windows_base/variable IO/variable.c
@@ -0,0 +1,12 @@
+int a =1; //4B
+short b =1; //2
+long c =1; //4
+float d =1.1; //4
+double e =1.1; //8
+char f = 'a'; //1
+
+int main(int argc, char const *argv[])
+{
+	printf(a,b,c,d,e,f)
+	return 0;
+}
