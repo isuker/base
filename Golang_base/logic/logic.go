03231717363e10e91415e362334260295fcc0f86
gzj <987969625@qq.com>
2016-06-17 10:07:19 +0800

    temp

---
+++ Golang_base/logic/logic.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	x := 0
+	if x == 0 {
+		println(x)
+	}
+	for x < 10 {
+		print(x)
+		x++
+	}
+}
--- Golang_base/variable IO/variable.go
+++ Golang_base/variable IO/variable.go
@@ -1,16 +1,38 @@
 package main
 
+import (
+	"fmt"
+)
+
 //variable out of function body muset use "var"
 //var x int = 1
 var x = 1
-
-//other :=1
-//var a , b , c int
 var a, b, c = 1, 2, 3
+var slice []int
 
-//d,e,f := 1,2,3
+///golang struct
+type books struct {
+	xx string
+}
 
 func main() {
+	var scanf = 1
+	fmt.Scanf("%d", scanf)
 	d, e, f := 1, 2, 3
+	//var ss = [2]int{1, 2}
+
+	ss := [2]int{1, 2}
+	print(ss[0])
 	println(x, a, b, c, d, e, f)
+
+	//point
+	ip := &d
+	println(ip)
+
+	var ip2 = &d
+	println(ip2)
+
+	slice = []int{1, 2, 3}
+	println(slice[0])
+
 }
