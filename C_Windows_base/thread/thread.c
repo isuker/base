47e36c74d6a119e2a652b5c60fd5215477e1dca5
gzj <987969625@qq.com>
2016-06-24 16:01:48 +0800

    temp

---
+++ C_Windows_base/thread/thread.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <process.h>
+#include <windows.h>
+
+DWORD WINAPI  thread1()
+{
+	while(1)
+	{
+		printf("I'm first\n" );
+		sleep(1);
+	}
+	
+	return 0;
+}
+DWORD WINAPI thread2()
+{
+	while(1)
+	{
+	  printf("I'm second\n");
+	  sleep(1);
+	}
+	return 0;
+}
+
+int main ()
+{
+   HANDLE h1 = CreateThread(NULL, 0, thread1, NULL, 0, NULL);  
+   HANDLE h2 = CreateThread(NULL, 0, thread2, NULL, 0, NULL);
+   WaitForSingleObject(h1, INFINITE);
+   WaitForSingleObject(h2, INFINITE);
+   return 0;
+}
