13815a695952be5bbdfe43f45d636eeb5761c422
gzj <987969625@qq.com>
2016-07-11 10:26:29 +0800

    t

--- Python_base/http/test.py
+++ Python_base/http/test.py
@@ -1,14 +1,17 @@
 import urllib.request
 import re
 
+src = open("./src.txt",'wb+')
+tar = open("./tar.txt",'w')
+
 fp = urllib.request.urlopen("http://nba.hupu.com/")
 f = fp.read()
-src = open("./src.txt",'wb')
-tar = open("./tar.txt",'wb')
-
-reg_str = '^http.*html$'
-html_str = re.match(reg_str,str(f))
-
-tar.write(html_str)
 src.write(f)
+
+html_str = re.search(r'http.*?html',str(f))
+
+##print(type(html_str.group()))
+print(type(f))
+
 src.close()
+tar.close()
